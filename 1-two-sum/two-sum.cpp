300520527604293407ee4ce0458e2683388996c0
SUDIPTA KUMAR NATH <nathsudipta7@gmail.com>
2023-09-10 21:45:21 +0530

    two-sum.cpp
    Code updated: removed unnecessary initialization

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,15 +5,13 @@ public:
         int s= nums.size();
         map<int,int> t;
         for(int i=0;i<s;i++){
-            t[nums[i]]=i;
-        }
-        for(int i=0;i<s;i++){
             auto it= t.find(target-nums[i]);
-            if(it!=t.end() && it->second!=i){
+            if(it!=t.end()){
                 res.push_back(i);
                 res.push_back(it->second);
                 return res;
             }
+             t[nums[i]]=i;
         }
         return res;
     }
