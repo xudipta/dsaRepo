bcc1053de912450e609145376474239e672fff1b
SUDIPTA KUMAR NATH <nathsudipta7@gmail.com>
2023-09-11 23:35:59 +0530

    Time: 6 ms (25.79%) | Memory: 6.3 MB (70.52%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        map<char,int> m={{'0',0},{'1',1},{'2',2},{'3',3},
-        {'4',4},{'5',5},{'6',6},{'7',7},{'8',8},{'9',9}};
-
         int res=0;
         int len=s.length();
         int mul=1;
         int i=0;
+        //skip leading spaces
         while(s[i]==' '){
             i++;
         }
+        //check for +/-
         if(s[i]=='-' || s[i]=='+'){
             if(s[i]=='-') mul=-1;
             if(s[i]=='+') mul=1;
@@ -18,15 +17,13 @@ public:
         }
         
         for(;i<len;i++){
-            
             if(isdigit(s[i])){
-                if(res>INT_MAX/10 || res==INT_MAX/10 && m[s[i]]>7 ){
-                    if(mul==-1){
-                        return INT_MIN; 
-                    }else return INT_MAX;
+                //check for overflow
+                if(res>INT_MAX/10 || res==INT_MAX/10 && (s[i]-'0')>7 ){
+                    if(mul==-1) return INT_MIN; 
+                    else return INT_MAX;
                 }
-                res=res*10+m[s[i]];
-                cout<<s[i]<<":"<<res<<"||";
+                res=res*10+(s[i]-'0');
             }else{
                 return mul*res;  
             }
